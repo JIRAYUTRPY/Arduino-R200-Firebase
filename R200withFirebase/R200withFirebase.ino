1349cb5b828d99674de9a22178fb5d10377979ad
jirayut <jirayutrpy@hotmail.com>
2023-12-14 19:47:02 +0700

    feat:set up firebase and fix:resolve bug

--- R200withFirebase/R200withFirebase.ino
+++ R200withFirebase/R200withFirebase.ino
@@ -1,23 +1,22 @@
-#include <ESP8266WiFi.h>
-#include <FirebaseArduino.h>
+#include "ESP8266WiFi.h"
+#include "FirebaseESP8266.h"
 
 #define FIREBASE_HOST ""
 #define FIREBASE_AUTH ""
 #define WIFI_SSID ""
 #define WIFI_PASSWORD ""
-#define controller_baud_rate 1152000
-
+FirebaseData firebaseDataReader;
 ///****** R200 Commamnd *******///
 //*Inventory
 unsigned char ReadMulti[10] =   {0XAA,0X00,0X27,0X00,0X03,0X22,0XFF,0XFF,0X4A,0XDD};
 //*Mode
-unsigned char DeseMode[8] =     {0XAA,0X00,0XF5,0X00,0X01,0X01,0XF7,0XDD};
+unsigned char DenseMode[8] =     {0XAA,0X00,0XF5,0X00,0X01,0X01,0XF7,0XDD};
 //*RFCH
 unsigned char RFCH90225MHz[8] = {0XAA,0X00,0XAB,0X00,0X01,0X00,0XAC,0XDD};
 //*Region
 unsigned char USRegion[8] =     {0XAA,0X00,0X07,0X00,0X01,0X02,0X0A,0XDD};
 //*PA Power
-unsigned char PA185DBM[8] =     {0XAA,0X00,0XB6,0X00,0X02,0X07,0XA3,0XF9,0XDD};
+unsigned char PA185DBM[9] =     {0XAA,0X00,0XB6,0X00,0X02,0X07,0XA3,0XF9,0XDD};
 ///****************************///
 
 //********** Env Parameter **********//
@@ -28,13 +27,19 @@ unsigned int incomedate = 0;
 unsigned int parState = 0;
 unsigned int codeState = 0;
 unsigned int totalTag = 88;
+unsigned int moduleNumber = 2;
+String path = "/DATA";
+String lastEpc = "";
+String state = "init";
+String mechineNumber = "1";
+bool display = false;
 //***********************************//
 
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
-  Serial.begin(controller_baud_rate);
+  Serial.begin(112500);
   Serial.println("R200 with Firebase - Bed tracking.");
-  Serial.println("************* WIFI Sys Init **************")
+  Serial.println("************* WIFI Sys Init **************");
   WiFi.mode(WIFI_STA);
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
   Serial.print("connecting");
@@ -45,47 +50,85 @@ void setup() {
   Serial.println();
   Serial.print("connected: ");
   Serial.println(WiFi.localIP());
-  Serial.println("************* Firebase Interface Init **************")
+  Serial.println("************* Firebase Interface Init **************");
   Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+  Firebase.reconnectWiFi(true);
+  if (!Firebase.beginStream(firebaseDataReader, path + "/"))
+  {
+      Serial.println("Could not begin stream");
+      Serial.println("REASON: " + firebaseDataReader.errorReason());
+      Serial.println();
+  }
   Serial.println("");
-  Serial.pringln("***************  Default setting  ***************");
-  Serial.pringln("Controller : Buad Rate '%d'", controller_baud_rate);  
+  Serial.println("***************  Default setting  ***************");
+  Serial.println("Controller : Buad Rate '115200'");  
   Serial.println("R200       : Mode 'Dense'");
   Serial.write(DenseMode,8);
   Serial.println("           : Region 'US'");
   Serial.write(USRegion,8);
   Serial.println("           : PA Power '18.5dBm'");
   Serial.write(PA185DBM,8);
-  Serial.pringln("           : RFCH '902.25MHz'");
+  Serial.println("           : RFCH '902.25MHz'");
   Serial.write(RFCH90225MHz,8);
   Serial.print("             : Command -> ReadMulti '");
   for(int x = 0 ; x < 10 ; x++){
-    Serial.print("%c", ReadMulti[x])
+    Serial.print(ReadMulti[x], HEX);
     Serial.print(" ");
     if(x == 9){
       Serial.println("'");
     }
+  }
 }
 
 void loop() {
-  timeSec ++ ;
-  if(timeSec >= 50000){
-    timemin ++;
-    timeSec = 0;
-    if(timemin >= 20){
-      timemin = 0;
-      digitalWrite(LED_BUILTIN, HIGH);
-      Serial.write(ReadMulti,10);
-      digitalWrite(LED_BUILTIN, LOW); 
+  if(state == "init"){
+    changeState();
+  }else if(state == "db"){
+    if(Firebase.ready()){
+      
+    }else{
+      Serial.println("Firebase not ready");
+    }
+  }else if(state == "read"){    
+    lastEpc = read();
+    if(lastEpc != "wait" || lastEpc != ""){
+      Serial.println(lastEpc);
     }
   }
-  multipleReading();
 }
 
-void multipleReading(){
-  if(Serial.available() > 0)
-  {
-	
+void changeState(){
+  if(Serial.available() > 0){
+      display = true;
+      state = Serial.read();          
+  }
+  if(state == "init" && display == true){
+    Serial.println("Current State : init | Select State working: db or read");
+    display = false;
+  }
+  if(state == "db" && display == true){
+    Serial.println("Current State :  db  | Select State working: read");
+    display = false;
+  }
+}
+
+
+String read(){
+  timeSec ++ ;
+    if(timeSec >= 50000){
+      timemin ++;
+      timeSec = 0;
+      if(timemin >= 20){
+        timemin = 0;
+        Serial.write(ReadMulti,10);
+      }
+    }
+  return multipleReading();
+}
+
+String multipleReading(){
+  String epc = "";
+  if(Serial.available() > 0){
     incomedate = Serial.read();
     if((incomedate == 0x02)&(parState == 0))
     {
@@ -99,13 +142,13 @@ void multipleReading(){
       dataAdd ++;
       if(dataAdd == 6)
       {
-        Serial.print("RSSI:"); 
-        Serial.println(incomedate, HEX); 
+        // Serial.print("RSSI:"); 
+        // Serial.println(incomedate, HEX); 
         }
        else if((dataAdd == 7)|(dataAdd == 8)){
         if(dataAdd == 7){
-          Serial.print("PC:"); 
-          Serial.print(incomedate, HEX);
+          // Serial.print("PC:"); 
+          // Serial.print(incomedate, HEX);
         }
         else {
            Serial.println(incomedate, HEX);
@@ -113,9 +156,12 @@ void multipleReading(){
        }
        else if((dataAdd >= 9)&(dataAdd <= 20)){
         if(dataAdd == 9){
-          Serial.print("EPC:"); 
-        }        
-        Serial.print(incomedate, HEX);
+          Serial.print("EPC:");
+          epc = String(incomedate, HEX);
+        }else{
+          Serial.print(incomedate, HEX);
+          epc = epc + " " + String(incomedate, HEX);
+        }
        }
        else if(dataAdd >= 21){
         Serial.println(" "); 
@@ -129,5 +175,7 @@ void multipleReading(){
       parState = 0;
       codeState = 0;
     }
+    return epc;
   }
+  return "wait";
 }
